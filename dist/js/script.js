c4555a9484dd7947cad239da776ae30e039ab606
Uliana <olecsyn@gmail.com>
2021-07-10 15:29:50 +0200

    Form with question-random without-answer

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,13 +9,17 @@ window.addEventListener('DOMContentLoaded', ()=>{
           modal_dialog = document.querySelector(".modal__dialog"),
           button_send = document.getElementById("send"),
           modal_thanks = document.querySelector(".modal-thanks__content"),
+          form = document.querySelector(".modal__form"),
           form_name = document.querySelector("input[name='name']"),
           form_email = document.querySelector("input[name='email']"),
           form_phone = document.querySelector("input[name='phone']"),
-          form_text = document.querySelector("textarea[name='text']");
+          form_text = document.querySelector("textarea[name='text']"),
+          form_questions = document.querySelector(".form__questions");
+         
+    
+    let random;  
 
-
-        //   new WOW().init();
+          new WOW().init();
 
     function showModal(){
         modal.classList.add("modal_active");
@@ -67,11 +71,112 @@ window.addEventListener('DOMContentLoaded', ()=>{
     button_send.addEventListener("click", (event)=>{
         
         if (form_name.validity.valid && form_email.validity.valid && form_phone.validity.valid && form_text.validity.valid){
-            alert("jf");
         showThanksModal();
         event.preventDefault();
         }
     });
+
+    // form questions
+    class Questions{
+        constructor(quest, answer1,  answer1_boolean, answer2, answer2_boolean, answer3, answer3_boolean, class_name){
+            this.quest = quest;
+            this.answer1 = answer1;
+            this.answer1_boolean = answer1_boolean;
+            this.answer2 = answer2;
+            this.answer2_boolean = answer2_boolean;
+            this.answer3 = answer3;
+            this.answer3_boolean = answer3_boolean;
+            this.class_name = class_name;
+        }
+
+        writeContent(){
+            const div = document.createElement("div");
+            div.classList.add("form__questions", `${this.class_name}`);
+            div.innerHTML = 
+            // <legend> Nie jestem robotem </legend>
+           ` <p>${this.quest}</p>
+
+            <div class="form__wrapper-answers">
+            <label for="${this.answer1}">${this.answer1}
+            <input type="radio" id="${this.answer1}" name="drone" value="${this.answer1}">
+            </label>
+            <label for="${this.answer2}">${this.answer2}
+            <input type="radio" id="${this.answer2}" name="drone" value="${this.answer2}">
+            </label>
+            <label for="${this.answer3}">${this.answer3}
+            <input type="radio" id="${this.answer3}" name="drone" value="${this.answer3}">
+            </label>
+            `;
+           form.append(div);
+          
+        }
+    }
+
+    const quest_1 = new Questions(
+        'Beethovena, Mozarta, Bacha i Chopina łączy myzyka:',
+        'klasyczna',
+        'true',
+        'pop',
+        'false',
+        'rock',
+        'false',
+        '1'
+    );
+    // quest_1.writeContent();
+
+    const quest_2 = new Questions(
+        'Muzyka popularna, najbardziej rozpowszechniona:',
+            'folk',
+            'false',
+            'pop',
+            'true',
+            'reggae',
+            'false',
+            '2'
+        );
+
+    const quest_3 = new Questions(
+        'Taneczna, łatwa w odbiorze muzyka:',
+            'rap',
+            'false',
+            'jazz',
+            'false',
+            'disco',
+            'true',
+            '3'
+        );
+    // quest_3.writeContent();
+
+    const quest_4 = new Questions(
+        'Uroczysta pieśń, oficjalny narodowy symbol państwa:',
+            'flag',
+            'false',
+            'godło',
+            'false',
+            'hymn',
+            'true',
+            '4'
+        );
+    // quest_4.writeContent();
+
+    random = Math.floor(Math.random() * 4) + 1;
+     switch (random) {
+        case 1:
+            quest_1.writeContent();
+          break;
+        case 2:
+            quest_2.writeContent();
+            break;
+        case 3:
+            quest_3.writeContent();
+            break;
+        default:
+            quest_4.writeContent();
+            break;
+     }
+    
+
+
     
 
 
